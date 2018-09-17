cef7a4d0d026a8f941904b5aa897fc1f7499ced8
GilmarBec <trabalhos4.5.1.4@gmail.com>
2018-09-17 19:23:59 -0300

    add mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,12 +27,24 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
   # mailer devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 456,
+    domain: "gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "restartinging@gmail.com",
+    password: "Restarting2018"
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
